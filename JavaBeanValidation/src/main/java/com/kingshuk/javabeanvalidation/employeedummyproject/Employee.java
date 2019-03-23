02e2b2b24ffecd10932721adc261f84bef6743e0
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-23 00:27:19 -0500

    Committing a project for JSR java bean validation

---
+++ JavaBeanValidation/src/main/java/com/kingshuk/javabeanvalidation/employeedummyproject/Employee.java
@@ -0,0 +1,31 @@
+package com.kingshuk.javabeanvalidation.employeedummyproject;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+
+	@NotNull(message = "MyMessaage: Name can't be null")
+	@Size(max = 10, message="Name can't be larger than 10")
+	private String name;
+
+	@NotNull(message = "MyMessaage: Email address can't be null")
+	@Size(max= 20, message="Email address can't be larger than 20")
+	//@Pattern(regexp=".+@.+\\.[a-z]+", message = "Please provide a valid email address")
+	@Email(message = "Please provide a valid email address")
+	private String email;
+
+}
---
+++ JavaBeanValidation/src/main/java/com/kingshuk/javabeanvalidation/employeedummyproject/EmployeeValidationTestHarness.java
@@ -0,0 +1,35 @@
+package com.kingshuk.javabeanvalidation.employeedummyproject;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+public class EmployeeValidationTestHarness {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+
+		EmployeeValidationTestHarness harness = new EmployeeValidationTestHarness();
+
+		harness.printEmployee(employee);
+	}
+
+	public void printEmployee(Employee employee) {
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+
+		Validator validator = validatorFactory.getValidator();
+
+		Set<ConstraintViolation<Employee>> violations = validator.validate(employee);
+
+		if (violations.isEmpty()) {
+			System.out.println(employee.getName() + " has a valid object");
+		} else {
+			violations.forEach(violation -> System.out.println(violation.getMessage()));
+		}
+
+	}
+
+}
